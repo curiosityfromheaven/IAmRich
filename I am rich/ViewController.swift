58f3c387fd2a619c5f55b0607a3cd48cc3d8c820
Krzysztof Kaczynski <krzysztof.kaczynski@protonmail.com>
2021-02-05 04:16:24 +0100

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by user190258 on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
